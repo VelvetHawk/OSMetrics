0bd5878d1d91e53b7f1ce6b67b9257674de00ff2
VelvetHawk <artem.smnv@yahoo.ie>
2019-09-05 11:53:51 +0300

    Updated setup.py with additional packaging information

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+	long_description = file.read()
 
 setup(
 	name="OS Metrics",
 	version="0.1",
-	packages=['os_metrics'],
-	long_description=open('README.md').read()
+	author="Artem Semenov",
+	packages=find_packages(),
+	description="Package to collect OS metrics",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent"
+	],
+	python_requires='>=3.7'
 )
+
