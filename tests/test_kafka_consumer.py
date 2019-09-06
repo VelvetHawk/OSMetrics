924a08a65281e536cecb13dc2e9e6c57c1f7f219
VelvetHawk <artem.smnv@yahoo.ie>
2019-09-06 17:57:13 +0300

    Added tests\test_kafka_consumer.py with skeletal code

---
+++ tests/test_kafka_consumer.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestKafkaConsumer(unittest.TestCase):
+	def test_database_connection(self):
+		self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+	unittest.main()
