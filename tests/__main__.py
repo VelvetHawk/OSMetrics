7da490a6a75a8011317ce203465c7322ae0af21f
VelvetHawk <artem.smnv@yahoo.ie>
2019-09-06 17:56:41 +0300

    Added tests\__main__.py and code to run tests beginning with test_ prefix automatically

---
+++ tests/__main__.py
@@ -0,0 +1,11 @@
+import unittest
+
+# Code to automatically run all tests sourced from:
+# https://stackoverflow.com/questions/1732438/how-do-i-run-all-python-unit-tests-in-a-directory
+
+loader = unittest.TestLoader()
+start_dir = '.\\tests'
+suite = loader.discover(start_dir, pattern="test_*.py")
+
+runner = unittest.TextTestRunner()
+runner.run(suite)
