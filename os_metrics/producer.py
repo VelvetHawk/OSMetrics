626d4944579f8e737a884fd959ea4d127f89aa8d
VelvetHawk <artem.smnv@yahoo.ie>
2019-09-08 19:06:12 +0300

    Added register_machine() function to os_metrics\producer.py to send an initial registration call for the current system with various matrics. Also switched to using the MAC address of each machine as unique identifier

--- os_metrics/producer.py
+++ os_metrics/producer.py
@@ -4,11 +4,18 @@
 # Producer code sourced from:
 # https://help.aiven.io/en/articles/489572-getting-started-with-aiven-kafka
 
+# MAC address code sourced from:
+# https://www.geeksforgeeks.org/extracting-mac-address-using-python/
+
 import platform
 import json
+import uuid
+import re
 
 import psutil
 import kafka
+
+from config import logger
 
 
 class Producer:
@@ -17,20 +24,23 @@ class Producer:
 	topic about metrics from the current operating system.
 	"""
 
-	def __init__(self, host, port, ca_file, cert_file, key_file, kafka_topic):
+	def __init__(self, client_id, host, port, ca_file, cert_file, key_file, kafka_topic):
 		# Set up kafka producer
 		self.producer = kafka.KafkaProducer(
 			bootstrap_servers="{}:{}".format(host, port),
+			client_id=client_id,
 			security_protocol="SSL",
 			ssl_cafile=ca_file,
 			ssl_certfile=cert_file,
 			ssl_keyfile=key_file,
 		)
+		self.client_id = client_id
 		self.kafka_topic = kafka_topic
 		self.cpu_percentages = None
 		self.memory_percentages = None
 
 		# Get basic OS information
+		self.mac_id = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
 		self.os_name = platform.system()
 		self.os_release = platform.release()
 		self.os_architecture = "{} ({})".format(platform.architecture()[0], platform.machine())
@@ -56,18 +66,49 @@ class Producer:
 		"""
 		self.memory_percentages = [psutil.virtual_memory().percent, psutil.swap_memory().percent]
 
+	def register_machine(self):
+		"""
+		Sends a message to the Kafka topic about the current machine. Uses the
+		'registration' type to let the consumer know that this is a new entry
+		"""
+		message = {
+			'type': 'registration',
+			'content': {
+				'mac_id': self.mac_id,
+				'producer_id': self.client_id,
+				'os_name': self.os_name,
+				'os_release': self.os_release,
+				'os_architecture': self.os_architecture,
+				'os_version': self.os_version,
+				'processor': self.processor,
+				'cpu_physical_cores': self.cpu_physical_cores,
+				'cpu_logical_cores': self.cpu_logical_cores,
+				'total_ram': self.total_ram,
+				'total_swap_space': self.total_swap_space
+			}
+		}
+		logger.debug("Sending message: {}".format(message))
+		self.producer.send(self.kafka_topic, json.dumps(message).encode("utf-8"))
+		self.producer.flush()
+
 	def send_metrics(self):
 		"""
-		Collect metrics and send them to the given Kafka Topic
+		Collect metrics and send them to the given Kafka Topic. Uses
+		'log' type to send log messages.
 		"""
 		self.get_cpu_metrics()
 		self.get_memory_metrics()
 		message = {
-			'cpu_metrics': self.cpu_percentages,
-			'memory_metrics': self.memory_percentages
+			'type': 'log',
+			'content': {
+				'mac_id': self.mac_id,
+				'producer_id': self.client_id,
+				'cpu_metrics': self.cpu_percentages,
+				'memory_metrics': self.memory_percentages
+			}
 		}
 		self.producer.send(self.kafka_topic, json.dumps(message).encode("utf-8"))
-		print(message)
+		self.producer.flush()
 
 	def stop(self):
 		self.producer.close(timeout=None)
