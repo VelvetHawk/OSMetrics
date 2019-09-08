049ebe4fa82d8343bd9851f805ecabf8eb67d34c
VelvetHawk <artem.smnv@yahoo.ie>
2019-09-08 19:02:50 +0300

    Added Producer and Consumer IDs in config.py as well as implementing logging functionality

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+import sys
+import logging
 
+# Set up environment variables
+KAFKA_PRODUCER_ID = os.getenv("KAFKA_PRODUCER_ID")
+KAFKA_CONSUMER_ID = os.getenv("KAFKA_CONSUMER_ID")
 KAFKA_HOST = os.getenv("KAFKA_HOST")
 KAFKA_PORT = os.getenv("KAFKA_PORT")
 KAFKA_SSL_CA_FILE = os.getenv("KAFKA_SSL_CA_FILE")
@@ -16,3 +21,18 @@ PG_SSL_CA_FILE = os.getenv("PG_SSL_CA_FILE")
 PG_SERVICE_URI = "postgres://{}:{}@{}:{}/{}?sslmode=require".format(
 	PG_DB_USER, PG_USER_PASSWORD, PG_HOST, PG_PORT, PG_DB_NAME
 )
+
+
+# Set up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Format Log messages
+formatter = logging.Formatter('%(asctime)s [%(name)s] [%(levelname)s]: %(message)s')
+
+# Set up a handler to print to  the console
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(formatter)
+
+# Set logger handler
+logger.addHandler(handler)
