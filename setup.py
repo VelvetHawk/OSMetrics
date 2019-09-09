b49d49172989884db39b61bbd6840790c73834e9
VelvetHawk <artem.smnv@yahoo.ie>
2019-09-09 10:48:50 +0300

    Install_requires now takes the data from requirements.txt for dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 import setuptools
 
-with open("README.md", "r") as file:
-	long_description = file.read()
+with open("README.md", "r") as readme_file:
+	long_description = readme_file.read()
+
+# Code sourced from:
+# https://stackoverflow.com/questions/1706198/python-how-to-ignore-comment-lines-when-reading-in-a-file
+# This is to avoid have to keep both requirements.txt AND install_requires in sync
+with open('requirements.txt') as requirements_file:
+	requirements = requirements_file.read().splitlines()
+	for requirement in requirements:
+		if requirement.startswith("#"):
+			requirements.remove(requirement)
 
 setuptools.setup(
 	name="OS Metrics",
@@ -15,6 +24,12 @@ setuptools.setup(
 		"Programming Language :: Python :: 3",
 		"Operating System :: OS Independent"
 	],
-	python_requires='>=3.7'
+	python_requires='>=3.7',
+	install_requires=[
+		# Hello
+		"",
+		# Hi
+		""
+	]
 )
 
